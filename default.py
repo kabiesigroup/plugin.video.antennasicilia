2893ff40dce02bc2e008427db400e67f4e12f107
Ademola Taofik <39627448+kabiesigroup@users.noreply.github.com>
2018-05-26 23:12:49 +0100

    Update default.py

--- default.py
+++ default.py
@@ -6,7 +6,7 @@ addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'Live TV')
 
-url='rtmp://live.lasiciliaweb.it:1935/tg/as'
+url='http://185.105.4.188:1935/ltv/myStream/playlist.m3u8'
 
 li = xbmcgui.ListItem('Live TV', iconImage='icon.pgn')
 
